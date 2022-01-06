9698595170ba9e79fcbbcd7d686dc3b80ebbf1ef
stirbuestera09@yahoo.com <stirbuestera09@yahoo.com>
2022-01-06 17:26:25 +0100

    homework7

---
+++ homework07/app.js
@@ -0,0 +1,77 @@
+// get element from DOM : inputs, textarea and alert div and submit button
+var alertMessage = document.getElementById('alert');
+var inputFirstName = document.getElementById('firstName');
+var inputLastName = document.getElementById('lastName');
+var inputGender = document.getElementsByName('gender');
+var textAreaMessage = document.getElementById('message');
+var submitBtnForm = document.querySelector('.btn-submit');
+
+// add event listener to inputs&textarea and button
+inputFirstName.addEventListener('keyup', checkInputFirstNameFunction);
+inputLastName.addEventListener('keyup', checkInputLastNameFunction);
+textAreaMessage.addEventListener('keyup', checkTextAreaMessageFunction);
+submitBtnForm.addEventListener('click', submitForm);
+
+function checkInputFirstNameFunction(e){
+    // console.log(e.target);
+    if(e.target.value == 0){
+        // console.log('is empty');
+        inputFirstName.classList.add('is-invalid');
+    } else {
+        inputFirstName.classList.remove('is-invalid');
+    }
+}
+
+function checkInputLastNameFunction(e){
+    if(e.target.value == 0){
+        inputLastName.classList.add('is-invalid');
+    } else {
+        inputLastName.classList.remove('is-invalid');
+    }
+}
+
+function checkTextAreaMessageFunction(e){
+    if(e.target.value == 0){
+        textAreaMessage.classList.add('is-invalid');
+    } else {
+        textAreaMessage.classList.remove('is-invalid');
+    }
+    if (e.keyCode == 13) {
+        submitForm(e);
+    }
+    e.preventDefault();
+}
+
+function submitForm(e){
+    if  ( 
+        inputFirstName.value == 0 ||   
+        inputLastName.value == 0 || 
+        textAreaMessage == 0 
+        )
+    {
+        inputFirstName.classList.add('is-invalid');
+        inputLastName.classList.add('is-invalid');
+        textAreaMessage.classList.add('is-invalid');
+    } 
+    else {    
+        inputFirstName.classList.remove('is-invalid');
+        inputLastName.classList.remove('is-invalid');
+        textAreaMessage.classList.remove('is-invalid');  
+            alertMessage.classList.add('alert-success', 'd-block', 'show' ,'fade');
+            alertMessage.innerHTML = 
+            "<span class='check'>✓</span>" +
+            "Thank you for contacting us, " +
+                inputFirstName.value +
+                "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" + 
+                "</button>";
+            console.log('Firstname : ' + inputFirstName.value);
+            console.log('Lastname : ' + inputLastName.value);
+            for (i = 0; i < inputGender.length; i++) {
+                if (inputGender[i].checked) {
+                    console.log('Gender : ' + inputGender[i].value);       
+                }        
+            }  
+            console.log('Message : ' + textAreaMessage.value);
+    }
+    e.preventDefault();
+}
